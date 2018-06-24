7ba7beeefb216c70d046c7c6e3bc75d3effcf150
moltaz5 <40433002+moltaz5@users.noreply.github.com>
2018-06-24 13:47:46 +0200

    Add files via upload

--- Projekt - Luffarschack.py
+++ Projekt - Luffarschack.py
@@ -18,26 +18,25 @@ speed(0)
 
 
 possible_choices={
-"ul":(start_x, start_y), "um":(start_x+100, start_y), "ur": (start_x+200, start_y),
+"ul":(start_x, start_y), "um":(start_x+100, start_y),
+"ur": (start_x+200, start_y),
 "ml":(start_x, start_y-100), "mm": (start_x+100, start_y-100),
 "mr":(start_x+200, start_y-100), "dl":(start_x, start_y-200),
 "dm":(start_x+100, start_y-200), "dr":(start_x+200, start_y-200)}
 
 
-winning_moves=[{possible_choices["ul"], possible_choices["um"], possible_choices["ur"]},
-               {possible_choices["ml"], possible_choices["mm"], possible_choices["mr"]},
-               {possible_choices["dl"], possible_choices["dm"], possible_choices["dr"]},
-               {possible_choices["ul"], possible_choices["ml"], possible_choices["dl"]},
-               {possible_choices["um"], possible_choices["mm"], possible_choices["dm"]},
-               {possible_choices["ur"], possible_choices["mr"], possible_choices["dr"]}]
-
+winning_moves=[{"ul", "um", "ur"},
+               {"ml", "mm", "mr"},
+               {"dl", "dm", "dr"},
+               {"ul", "ml", "dl"},
+               {"um", "mm", "dm"},
+               {"ur", "mr", "dr"}]
 
 
 
 #Skapar spelplanen
 
 forward(300)
-print(pos())
 penup()
 right(90)
 forward(100)
@@ -56,7 +55,6 @@ forward(100)
 rt(90)
 pendown()
 forward(300)
-
 rt(90)
 fd(300)
 rt(90)
@@ -76,7 +74,7 @@ fd(300)
 def write_a_cross(xy):
     color("red")
     penup()
-    goto(xy[0], xy[1])
+    goto(possible_choices[xy][0], possible_choices[xy][1])
     pendown()
     seth(315)
     fd(141.4)
@@ -92,7 +90,7 @@ def write_a_cross(xy):
 def write_a_circle(xy):
     color("blue")
     penup()
-    goto(xy[0], xy[1])
+    goto(possible_choices[xy][0], possible_choices[xy][1])
     seth(277)
     fd(60)
     pendown()
@@ -105,8 +103,11 @@ def write_a_circle(xy):
 #Nu till själva spelet
 
 
+condition_1= True
+condition_2= True
 
-while True:
+
+while condition_1 and condition_2:
     print("Hej och välkomna till en omgång luffarschack!")
     print("Först som får tre cirklar/kryss i rad vinner.")
     print("Kontrollerna fungerar som följer. Översätt upp, mitten, ned samt")
@@ -116,34 +117,73 @@ while True:
     player_2_choices=[]
     choices_left=possible_choices
 
+    temp_set_1=set()
+    temp_set_2=set()    
+    
+
+    for i in winning_moves:
+        if i in player_1_choices:
+            print("Spelare 1 vann!")
+            condition_1= False
+        elif i in player_2_choices:
+            print("Spelare 2 vann!")
+            condition_2= False
+    
+
     while True:
 
-        val_1=possible_choices[input("Spelare 1, välj en ruta: ")]
+
+        for i in winning_moves:
+            if i in player_1_choices:
+                print("Spelare 1 vann!")
+                condition_1= False
+                break
+            elif i in player_2_choices:
+                print("Spelare 2 vann!")
+                condition_2= False
+                break
+
+        
+
+        val_1=input("Spelare 1, välj en ruta: ")
         if val_1 not in choices_left:
             print("Det där kan du tyvärr inte välja.")
             continue
         elif val_1 in choices_left:
             write_a_cross(val_1)
-            choices_left.remove(val_1)
-            
-            player_1_choices.append(val_1)
-
-        val_2=possible_choices[input("Spelare 2, välj en ruta: ")]
-        if val_2 not in choices_left:
-            continue
-            print("Det där kan du tyvärr inte välja")
-        else:
-            write_a_circle(val_2)
-            choices_left.remove(val_2)
-            player_2_choices.append(val_2)
+            choices_left.pop(val_1)
+            if len(player_1_choices)>0:
+                player_1_choices.pop()
+            temp_set_1.add(val_1)
+            player_1_choices.append(temp_set_1)
 
 
-        if player_1_choices in winning_moves:
-            print("Spelare 1 vann!")
-            break
-        elif player_2_choices in winning_moves:
-            print("Spelare 2 vann!")
-            break
 
+        for i in winning_moves:
+            if i in player_1_choices:
+                print("Spelare 1 vann!")
+                condition_1= False
+            elif i in player_2_choices:
+                print("Spelare 2 vann!")
+                condition_2= False
+
+
+
+
+        while True:
+            val_2=input("Spelare 2, välj en ruta: ")
+            if val_2 not in choices_left:
+                print("Det där kan du tyvärr inte välja")
+                continue
+            else:
+                write_a_circle(val_2)
+                choices_left.pop(val_2)
+                if len(player_1_choices)>0:
+                    player_1_choices.pop()
+                temp_set_2.add(val_2)
+                player_2_choices.append(temp_set_2)
+                break
+
+        
 
 
